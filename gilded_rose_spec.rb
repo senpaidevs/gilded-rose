f567a89dd72011fcfbe260bf1841a9cad27eb8bb
Néstor Salceda <nestor.salceda@gmail.com>
2015-05-21 18:47:37 +0200

    Refactoring of the last test

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -5,20 +5,18 @@ describe GildedRose do
 
   context "when updating quality" do
     context "when updating normal items" do
+      before(:each) do
+        @items = [Item.new(a_name, 10, 20)]
+
+        GildedRose.new(@items).update_quality()
+      end
+
       it "decreases sell in" do
-        items = [Item.new(a_name, 10, 20)]
-
-        GildedRose.new(items).update_quality()
-
-        expect(items[0].sell_in).to eq(9)
+        expect(@items[0].sell_in).to eq(9)
       end
 
       it "decreases item quality" do
-        items = [Item.new(a_name, 10, 20)]
-
-        GildedRose.new(items).update_quality()
-
-        expect(items[0].quality).to eq(19)
+        expect(@items[0].quality).to eq(19)
       end
     end
   end
